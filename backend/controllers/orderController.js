b715d4f2725dafb61b6463b180ab9ef9651db077
jayAnjana13 <jayanjana13@gmail.com>
2024-12-27 18:12:04 +0530

    Update orderController.js

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -50,8 +50,8 @@ console.log("4 line item is added")
     const session = await stripe.checkout.sessions.create({
       line_items: line_items,
       mode: "payment",
-      success_url: `${frontend_url}/verify?success=true&orderId=${newOrder._id}`,
-      cancel_url: `${frontend_url}/verify?success=false&orderId=${newOrder._id}`,
+      success_url: `${frontend_url}verify?success=true&orderId=${newOrder._id}`,
+      cancel_url: `${frontend_url}verify?success=false&orderId=${newOrder._id}`,
       
     });
     console.log("session done")
