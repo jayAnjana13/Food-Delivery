a8d4f3f61b175d1e675266aa97169e366ff2f4de
jayAnjana13 <jayanjana13@gmail.com>
2024-12-27 18:17:08 +0530

    Update Verify.jsx

--- frontend/src/pages/Verify/Verify.jsx
+++ frontend/src/pages/Verify/Verify.jsx
@@ -14,43 +14,43 @@ const Verify = () => {
   console.log(success, orderId);
 
   const verifyPayment = async () => {
-    console.log("url", url);
-    const response = await axios.post(url + "api/order/verify", {
-      success,
-      orderId,
-    });
-    console.log("one");
-    if (response.data.success) {
-      navigate("/myorders");
-    } else {
-      navigate("/");
-    }
-
-    // try {
-    //   if (!url) {
-    //     throw new Error(
-    //       "Base URL is undefined. Please check the StoreContext."
-    //     );
-    //   }
-
-    //   const apiUrl = new URL("api/order/verify", url).toString(); // Validate URL
-    //   console.log("Sending request to:", apiUrl);
-
-    //   const response = await axios.post(apiUrl, {
-    //     success,
-    //     orderId,
-    //   });
-
-    //   console.log("Response received:", response.data);
-    //   if (response.data.success) {
-    //     navigate("/myorders");
-    //   } else {
-    //     navigate("/");
-    //   }
-    // } catch (error) {
-    //   console.error("Error verifying payment:", error.message);
-    //   navigate("/error");
+    // console.log("url", url);
+    // const response = await axios.post(url + "api/order/verify", {
+    //   success,
+    //   orderId,
+    // });
+    // console.log("one");
+    // if (response.data.success) {
+    //   navigate("/myorders");
+    // } else {
+    //   navigate("/");
     // }
+
+    try {
+      if (!url) {
+        throw new Error(
+          "Base URL is undefined. Please check the StoreContext."
+        );
+      }
+
+      const apiUrl = new URL("api/order/verify", url).toString(); // Validate URL
+      console.log("Sending request to:", apiUrl);
+
+      const response = await axios.post(apiUrl, {
+        success,
+        orderId,
+      });
+
+      console.log("Response received:", response.data);
+      if (response.data.success) {
+        navigate("/myorders");
+      } else {
+        navigate("/");
+      }
+    } catch (error) {
+      console.error("Error verifying payment:", error.message);
+      navigate("/error");
+    }
   };
 
   useEffect(() => {
