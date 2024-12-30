b2aeca6246a5ca8afc6925a969f38b5a20d03579
jayAnjana13 <jayanjana13@gmail.com>
2024-12-30 17:30:46 +0530

    Update PlaceOrder.jsx

--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -37,19 +37,17 @@ const PlaceOrder = () => {
         orderItems.push(itemInfo);
       }
     });
-    console.log("orderItems", orderItems);
-
+  
     let orderData = {
       address: data,
       items: orderItems,
       amount: getTotalCartAmount() + 2,
     };
-    console.log("entered");
-
+    
     const response = await axios.post(url + "/api/order/place", orderData, {
       headers: { token },
     });
-    console.log("helo");
+   
     if (response.data.success) {
       const { session_url } = response.data;
       window.location.replace(session_url);
